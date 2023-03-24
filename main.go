3f89a2060620335d94626d6b112193ad569d1906
sjtommylee <sjtommylee@gmail.com>
2023-03-24 12:04:09 -0700

    structs defined

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/spf13/viper"
 )
 
 // define the openai choices struct
 
-type OpenAIChoices struct{}
+type OpenAIChoices struct {
+	Text         string
+	Index        uint8
+	Prob         *uint8
+	FinishReason string
+}
 
 // define the openai respones struct
-
-type OpenAIResponses struct{}
+type OpenAIResponses struct {
+	Id      *string
+	Object  *string
+	Created *uint64
+	Model   *string
+	Choices []OpenAIChoices
+}
 
 // define the openai request struct
-
-type OpenAIRequest struct{}
+type OpenAIRequest struct {
+	Prompt     string
+	Max_tokens uint64
+	// Temperature *float32
+}
 
 // main entry point
 func main() {
+	fmt.Println("client running")
 	// using viper to get the appropriate envs
 
 	// if there is no api key, throw an error
